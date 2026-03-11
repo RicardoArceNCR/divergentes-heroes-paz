ee30ad5aa6cfbce7b45c4b9b83fcedbb0bad04e7
RicardoArceNCR <116515519+RicardoArceNCR@users.noreply.github.com>
2026-03-11 08:34:11 -0600

    refactor: clean timeline docs, refine editorial styles and add local fonts

--- inc/class-assets.php
+++ inc/class-assets.php
@@ -16,7 +16,7 @@ class DHP_Assets {
         // Google Fonts
         wp_register_style(
             'dhp-fonts',
-            'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Staatliches&display=swap',
+            'https://fonts.googleapis.com/css2?family=Lacquer&family=Red+Hat+Mono:wght@400;500;700&display=swap',
             [],
             null
         );
